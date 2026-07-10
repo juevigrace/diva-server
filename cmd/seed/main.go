85d717d4569c69c2970d4232e51ff026860c16f2
juevigrace <juev1grace@gmail.com>
2026-07-10 09:17:15 -0400

    fix seed

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/google/uuid"
+	"github.com/juevigrace/diva-server/internal/core/permission"
+	"github.com/juevigrace/diva-server/internal/core/session"
+	"github.com/juevigrace/diva-server/internal/core/user"
 	"github.com/juevigrace/diva-server/internal/models"
+	"github.com/juevigrace/diva-server/internal/models/dtos"
+	"github.com/juevigrace/diva-server/server"
 	"github.com/juevigrace/diva-server/storage"
+	"github.com/juevigrace/diva-server/storage/postgres"
+	"github.com/juevigrace/diva-server/storage/sqlite"
+
+	"github.com/joho/godotenv"
 )
 
 type seedPermission struct {
@@ -56,4 +66,86 @@ func seedAllPermissions(ctx context.Context, store storage.PermissionStore) {
 	}
 
 	log.Println("seeded all permissions")
+}
+
+func main() {
+	dev := flag.Bool("d", false, "Load .env.dev instead of .env")
+	s := flag.Bool("s", false, "Use SQLite (default)")
+	sqliteFlag := flag.Bool("sqlite", false, "Use SQLite (default)")
+	p := flag.Bool("p", false, "Use PostgreSQL")
+	postgresFlag := flag.Bool("postgres", false, "Use PostgreSQL")
+	flag.Parse()
+
+	useSQLite := *s || *sqliteFlag
+	usePG := *p || *postgresFlag
+
+	if usePG && useSQLite {
+		log.Fatal("only one database engine can be selected")
+	}
+	if !usePG && !useSQLite {
+		useSQLite = true
+	}
+
+	envFile := ".env"
+	if *dev {
+		envFile = ".env.dev"
+	}
+
+	if err := godotenv.Load(envFile); err != nil {
+		log.Fatal(err)
+	}
+
+	var database storage.Storage
+	var err error
+
+	if usePG {
+		dbCfg := postgres.NewPGConf()
+		dbCfg.LoadFromEnv()
+		database, err = postgres.New(dbCfg.(*postgres.PGConf))
+	} else {
+		dbCfg := sqlite.NewSQLiteConf()
+		dbCfg.LoadFromEnv()
+		database, err = sqlite.New(dbCfg.(*sqlite.SQLiteConf))
+	}
+	if err != nil {
+		log.Fatal("failed to create storage: %w", err)
+	}
+
+	serverConf := server.NewServerConfig().(*server.ServerConfig)
+	serverConf.LoadFromEnv()
+
+	pModule := permission.NewPermissionModule(database.PermissionStore())
+	sModule := session.NewSessionModule(database.SessionStore())
+
+	seedAllPermissions(context.Background(), database.PermissionStore())
+
+	uModule := user.NewUserModule(
+		database.UserStore(),
+		database.UserActionStore(),
+		database.UserPermissionStore(),
+		database.UserPreferenceStore(),
+		database.UserProfileStore(),
+		database.UserStateStore(),
+		pModule.Repo,
+		sModule.Repo,
+		sModule.Handler,
+		nil,
+	)
+
+	userDto := dtos.CreateUserDto{
+		Email:    serverConf.RootEmail,
+		Username: serverConf.RootUsername,
+		Password: serverConf.RootPassword,
+	}
+
+	id, err := uModule.URepo.Create(context.Background(), &userDto)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := uModule.URepo.UpdateRole(context.Background(), models.ROLE_ADMIN, id); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(id)
 }
